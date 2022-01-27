af30ed5efc11ae4c997a270f9405ac99791695ca
Billy Kirk <30280510+billykirk01@users.noreply.github.com>
2022-01-27 10:16:46 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,10 +27,10 @@ ln -s ~/.vimrc ~/.config/nvim/init.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-source .zshrc
+chsh -s $(which zsh)
+
+. .zshrc
 
 cargo install exa
 
 ssh-keygen
-
-sudo reboot
