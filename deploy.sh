50691e7f87904080b548d592b23dfda98ef6d2a5
Billy Kirk <30280510+billykirk01@users.noreply.github.com>
2022-01-27 10:29:07 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ ln -s ~/.vimrc ~/.config/nvim/init.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $(whoami)
 
 . .zshrc
 
