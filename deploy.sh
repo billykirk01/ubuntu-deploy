2e2b44d13258fcace394cc4bc5e31dcc5fae9cf7
Billy Kirk <30280510+billykirk01@users.noreply.github.com>
2022-01-27 09:30:32 -0600

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+set -e
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y zsh neovim unzip network-manager rtl8821ce-dkms exa fd-find
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+wget https://raw.githubusercontent.com/billykirk01/ubuntu-deploy/main/starship.toml
+mv starship.toml ~/.confg/
+
+wget https://raw.githubusercontent.com/billykirk01/ubuntu-deploy/main/.zshrc
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+curl -fsSL https://deno.land/install.sh | sh
+
+wget "https://go.dev/dl/$(curl https://go.dev/VERSION\?m\=text).linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go*linux-amd64.tar.gz
+
+wget https://raw.githubusercontent.com/billykirk01/ubuntu-deploy/main/.vimrc
+mkdir -p ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+source .zshrc
+
+sudo reboot
