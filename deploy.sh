836cffff5f7f27f94e913f7d228d755eef69e4bc
Billy Kirk <30280510+billykirk01@users.noreply.github.com>
2022-01-27 10:56:21 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ mv starship.toml ~/.config/
 
 wget https://raw.githubusercontent.com/billykirk01/ubuntu-deploy/main/.zshrc
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 curl -fsSL https://deno.land/install.sh | sh
 
